1872a7857ac5a38c3e1f2355271fd6208c6fae74
yoshalangius <henkkooi11@gmail.com>
2015-11-16 14:19:46 +0100

    trying a fix

---
+++ Assets/_Scripts/Menu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	public void HowToPlay(GameObject howToPlay){
+		howToPlay.SetActive (true);
+	}
+
+	public void ChangeToScene(int sceneToChangeTo){
+		Application.LoadLevel (sceneToChangeTo);
+	}
+	void Update (){
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			Application.Quit ();
+		}
+	}
+}
